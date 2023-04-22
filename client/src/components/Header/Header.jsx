6832bb34874ff1f5d9a43b20646352e15cbdb797
Alex <yosya1989@gmail.com>
2023-04-22 20:30:28 +0200

    test-2

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import { HeaderMenu } from 'src/components/HeaderMenu/HeaderMenu';
-import { Logo } from 'src/UI/Logo/Logo';
+// import { Logo } from 'src/UI/Logo/Logo';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 import styles from 'src/styles/Header.module.scss';
 
 export const Header = () => {
   return (
     <div className={styles.Header__row}>
-      <Logo />
+      <TwitterIcon
+        color="primary"
+        fontSize="large"
+      />
       <HeaderMenu />
     </div>
   );
