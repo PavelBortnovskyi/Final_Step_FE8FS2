4ddca2291128648413839aea4b4c511d830c55ad
Oleksiy K <klt.kae.nao@gmail.com>
2023-05-01 15:41:30 +0200

    DTO & Facade

---
+++ src/main/java/app/dto/rs/UserModelResponse.java
@@ -0,0 +1,28 @@
+package app.dto.rs;
+
+import app.model.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+public class UserModelResponse {
+
+  private String fullName;
+  private String userTag;
+  private String email;
+  private LocalDate birthdate;
+  private String bio;
+  private String location;
+  private String avatarImgUrl;
+  private String headerImgUrl;
+  private boolean isVerified;
+  private Set<UserModel> following;
+  private Set<UserModel> followers;
+  private Set<Tweet> tweets;
+  private Set<Message> messages;
+  private Set<Chat> chat;
+  private Set<Chat> chats;
+  private TweetAction tweetAction;
+}
---
+++ src/main/java/app/facade/UserModelFacade.java
@@ -0,0 +1,8 @@
+package app.facade;
+
+import app.dto.rq.UserModelRequest;
+import app.dto.rs.UserModelResponse;
+import app.model.UserModel;
+
+public class UserModelFacade extends GeneralFacade<UserModel, UserModelRequest, UserModelResponse> {
+}
