1983229515dce8f1402f6971862abf77471c2069
Pavel Bortnovskyi <bortnovskyi.pavlo@gmail.com>
2023-05-09 23:21:07 +0300

    Swagger working

--- src/main/java/app/utils/WebAppConfig.java
+++ src/main/java/app/utils/WebAppConfig.java
@@ -12,7 +12,7 @@ public class WebAppConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/");
     registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 
