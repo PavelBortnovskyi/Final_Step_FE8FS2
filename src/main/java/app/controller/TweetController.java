26a1434daadd6a8e3043d970d2791ace77b5ee74
valanir <valanir.vin@gmail.com>
2023-05-12 16:32:38 +0300

    add controller for tweet

---
+++ src/main/java/app/controller/TweetController.java
@@ -0,0 +1,13 @@
+package app.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tweets")
+public class TweetController {
+}
