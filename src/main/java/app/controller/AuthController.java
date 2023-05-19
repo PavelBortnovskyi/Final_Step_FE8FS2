e90e4e28234fe60df0aacd9c586e0bd44548983b
Pavel Bortnovskyi <bortnovskyi.pavlo@gmail.com>
2023-05-19 19:40:50 +0300

    Some fixes

--- src/main/java/app/controller/AuthController.java
+++ src/main/java/app/controller/AuthController.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
--- src/main/java/app/exceptions/GeneralExceptionHandler.java
+++ src/main/java/app/exceptions/GeneralExceptionHandler.java
@@ -1,8 +1,6 @@
 package app.exceptions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
