a1a33889edadee6dca061d8efe5ead3819b2b879
Oleksiy K <klt.kae.nao@gmail.com>
2023-05-22 19:59:32 +0200

    Merge with Pavel refactor

--- src/main/java/app/dto/rq/UserModelRequest.java
+++ src/main/java/app/dto/rq/UserModelRequest.java
@@ -1,16 +1,14 @@
 package app.dto.rq;
 
 import app.annotations.Marker;
-import app.annotations.Views;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
 
 /**
  * UserDTO for login(Existed)/register(New) requests
@@ -25,7 +23,7 @@ public class UserModelRequest {
   @Null(groups = {Marker.Existed.class})
   private String fullName;
 
-  @JsonView(Marker.New.class)
+  @JsonView({Marker.New.class, Marker.Update.class})
   @ApiModelProperty(value = "User tag", example = "@john_doe", required = true, allowableValues = "range[2, 20]")
   @Size(max = 20, min = 2, message = "UserTag length must be in range 2..20 characters", groups = {Marker.Existed.class, Marker.New.class})
   @Null(groups = {Marker.Existed.class})
@@ -43,27 +41,27 @@ public class UserModelRequest {
   @Size(min = 8, max = 50, message = "Password length must be in range 8..50 characters", groups = {Marker.Existed.class, Marker.New.class})
   private String password;
 
-  @JsonView({Views.Update.class})
+  @JsonView({Marker.Update.class})
   @ApiModelProperty(value = "BirthDate", required = true)
   @PastOrPresent
   @NotNull
   private LocalDate birthDate;
 
-  @JsonView({Views.Update.class})
+  @JsonView({Marker.Update.class})
   @ApiModelProperty(value = "bio", required = true)
   @NotNull
   private String bio;
 
-  @JsonView({Views.Update.class})
+  @JsonView({Marker.Update.class})
   @ApiModelProperty(value = "location", required = true)
   @NotNull
   private String location;
 
-  @JsonView({Views.Update.class})
+  @JsonView({Marker.Update.class})
   @ApiModelProperty(value = "avatarImgMultipartFile", required = true)
   private MultipartFile avatarImgMultipartFile;
 
-  @JsonView({Views.Update.class})
+  @JsonView({Marker.Update.class})
   @ApiModelProperty(value = "HeaderImgMultipartFile", required = true)
   private MultipartFile headerImgMultipartFile;
 }
