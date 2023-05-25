c59070dd76a4f06b0f7ca66ff944a119bcfff0d0
Oleksiy K <klt.kae.nao@gmail.com>
2023-05-26 00:09:48 +0200

    Created universalCreated universal

--- src/main/java/app/service/CloudinaryService.java
+++ src/main/java/app/service/CloudinaryService.java
@@ -8,6 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 @Service
 @AllArgsConstructor
@@ -15,13 +20,24 @@ public class CloudinaryService {
 
   private final Cloudinary cloudinary;
 
-  public boolean isImage(MultipartFile file) {
-    String contentType = file.getContentType();
-    return contentType != null && contentType.startsWith("image/");
+  public void checkImageType(MultipartFile file) {
+    Map<String, ArrayList<Byte>> signatures = new HashMap<>();
+    signatures.put("*.jpeg, *.jpg", new ArrayList<>(Arrays.asList((byte) 0xFF, (byte) 0xD8)));
+    signatures.put("*.png", new ArrayList<>(Arrays.asList((byte) 0x89, (byte) 0x50, (byte) 0x4E, (byte) 0x47, (byte) 0x0D, (byte) 0x0A, (byte) 0x1A, (byte) 0x0A)));
+    try {
+      byte[] headerBytes = Arrays.copyOfRange(file.getBytes(), 0, 8);
+      if (signatures.values().stream()
+        .anyMatch(s -> IntStream.range(0, s.size())
+          .allMatch(i -> s.get(i).equals(headerBytes[i]))
+        )
+      ) return;
+    } catch (IOException ignored) {
+    }
+    throw new UploadImageException("Supported type: " + signatures.keySet());
   }
 
   public String uploadFile(MultipartFile file, String imgId) {
-    if (!isImage(file)) throw new UploadImageException("Not image.");
+    checkImageType(file);
     try {
       return cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap(
         "public_id", imgId,
