f5ce8fe5aa144f42d1bf0f3346e62840f7872c30
kostia89 <0665761301k@gmail.com>
2023-05-30 00:35:05 +0300

    hard cod user data

--- client/src/components/User/UserName.jsx
+++ client/src/components/User/UserName.jsx
@@ -2,6 +2,7 @@ import { Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 
 export function UserName() {
-  const fullName = useSelector((state) => state.user.user.fullName);
+  const fullName = "name";
+  // useSelector((state) => state.user.user.fullName);
   return <Typography variant="h4">{fullName}</Typography>;
 }
