086b96d183c8b37839607dfe40a34049dd1be8dc
Artem Shevchuk <art.shev00@gmail.com>
2023-06-03 15:13:34 +0200

    added likes, retweet and replyies

--- client/src/redux/thunk/getUserTweets.js
+++ client/src/redux/thunk/getUserTweets.js
@@ -8,7 +8,8 @@ export const getUserTweetsThunk = createAsyncThunk(
       const { data } = await myAxios.get(
         `/tweet/tweets/${userId}?page=${page}&pageSize=${pageSize}`
       );
-      return data;
+      console.log(data);
+      return data.content;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
