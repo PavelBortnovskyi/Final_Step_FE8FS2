c66563dc9a0fc15f946283bdb9e3fe98e0dab250
valanir <valanir.vin@gmail.com>
2023-06-05 16:29:46 +0300

    hardcore add bookmarks to db

--- src/main/resources/db/migration/V005_Bookmark_init.sql
+++ src/main/resources/db/migration/V005_Bookmark_init.sql
@@ -1,89 +1,32 @@
 INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 5, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.975519', null, TIMESTAMP '2023-05-30 18:45:37.975519', null, 'BOOKMARK', 8, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:38.108611', null, TIMESTAMP '2023-05-30 18:45:38.108611', null, 'BOOKMARK', 3, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:38.209233', null, TIMESTAMP '2023-05-30 18:45:38.209233', null, 'BOOKMARK', 1, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:38.304021', null, TIMESTAMP '2023-05-30 18:45:38.304021', null, 'BOOKMARK', 2, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:38.399350', null, TIMESTAMP '2023-05-30 18:45:38.399350', null, 'BOOKMARK', 3, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:38.534652', null, TIMESTAMP '2023-05-30 18:45:38.534652', null, 'BOOKMARK', 7, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:38.671466', null, TIMESTAMP '2023-05-30 18:45:38.671466', null, 'BOOKMARK', 7, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:38.807081', null, TIMESTAMP '2023-05-30 18:45:38.807081', null, 'BOOKMARK', 7, 3);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:38.963193', null, TIMESTAMP '2023-05-30 18:45:38.963193', null, 'BOOKMARK', 10, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:39.106848', null, TIMESTAMP '2023-05-30 18:45:39.106848', null, 'BOOKMARK', 11, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:39.240436', null, TIMESTAMP '2023-05-30 18:45:39.240436', null, 'BOOKMARK', 12, 3);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:39.375714', null, TIMESTAMP '2023-05-30 18:45:39.375714', null, 'BOOKMARK', 13, 4);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:39.510908', null, TIMESTAMP '2023-05-30 18:45:39.510908', null, 'BOOKMARK', 14, 5);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:39.645318', null, TIMESTAMP '2023-05-30 18:45:39.645318', null, 'BOOKMARK', 15, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:39.782866', null, TIMESTAMP '2023-05-30 18:45:39.782866', null, 'BOOKMARK', 16, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:39.949213', null, TIMESTAMP '2023-05-30 18:45:39.949213', null, 'BOOKMARK', 17, 3);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:40.086168', null, TIMESTAMP '2023-05-30 18:45:40.086168', null, 'BOOKMARK', 18, 4);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:40.220538', null, TIMESTAMP '2023-05-30 18:45:40.220538', null, 'BOOKMARK', 19, 5);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:40.355691', null, TIMESTAMP '2023-05-30 18:45:40.355691', null, 'BOOKMARK', 20, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:40.494714', null, TIMESTAMP '2023-05-30 18:45:40.494714', null, 'BOOKMARK', 21, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:40.629989', null, TIMESTAMP '2023-05-30 18:45:40.629989', null, 'BOOKMARK', 22, 3);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:40.774380', null, TIMESTAMP '2023-05-30 18:45:40.774380', null, 'BOOKMARK', 23, 4);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:40.911569', null, TIMESTAMP '2023-05-30 18:45:40.911569', null, 'BOOKMARK', 24, 5);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:41.046938', null, TIMESTAMP '2023-05-30 18:45:41.046938', null, 'BOOKMARK', 25, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:41.183532', null, TIMESTAMP '2023-05-30 18:45:41.183532', null, 'BOOKMARK', 26, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:41.323714', null, TIMESTAMP '2023-05-30 18:45:41.323714', null, 'BOOKMARK', 27, 3);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:41.465594', null, TIMESTAMP '2023-05-30 18:45:41.465594', null, 'BOOKMARK', 23, 1);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:41.601703', null, TIMESTAMP '2023-05-30 18:45:41.601703', null, 'BOOKMARK', 24, 2);
-
-INSERT INTO public.tweet_actions (id, created_at, created_by, updated_at, updated_by, action_type, tweet_id, user_id)
-VALUES (DEFAULT, TIMESTAMP '2023-05-30 18:45:41.737991', null, TIMESTAMP '2023-05-30 18:45:41.737991', null, 'BOOKMARK', 25, 3);
+VALUES
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 1, 3),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 2, 2),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 3, 1),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 4, 4),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 5, 5),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 6, 2),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 7, 4),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 8, 1),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 9, 5),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 10, 3),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 11, 1),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 12, 5),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 13, 4),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 14, 3),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 15, 2),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 16, 5),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 17, 1),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 18, 4),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 19, 2),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 20, 3),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 21, 5),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 22, 1),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 23, 4),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 24, 5),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 25, 2),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 26, 1),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 27, 3),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 28, 4),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 29, 5),
+    (DEFAULT, TIMESTAMP '2023-05-30 18:45:37.874406', null, TIMESTAMP '2023-05-30 18:45:37.874406', null, 'BOOKMARK', 30, 2);
