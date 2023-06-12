e9f6b758d2135124ea040688a09196b0ca4caf6d
valanir <valanir.vin@gmail.com>
2023-06-12 14:46:20 +0300

    repair count likes, add top50 algorithm

--- src/main/java/app/repository/RatingModelRepository.java
+++ src/main/java/app/repository/RatingModelRepository.java
@@ -4,9 +4,11 @@ import app.model.RatingModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RatingModelRepository extends JpaRepository<RatingModel, Long> {
   List<RatingModel> findAll();
   Page<RatingModel> findAll(Pageable pageable);
