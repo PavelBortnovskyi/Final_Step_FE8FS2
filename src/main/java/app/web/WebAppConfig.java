eaa85a0ca22b08b4f390f9ef0e8adbd22097041e
Pavel Bortnovskyi <bortnovskyi.pavlo@gmail.com>
2023-06-13 11:57:39 +0300

    WebAppCors option

--- src/main/java/app/web/WebAppConfig.java
+++ src/main/java/app/web/WebAppConfig.java
@@ -18,22 +18,29 @@ public class WebAppConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/chat-ws").allowedMethods("GET", "OPTIONS")
-      .allowedHeaders("*").allowedOrigins("http://localhost:3000", "https://final-step-fe-8-fs-2.vercel.app",
-        "http://localhost:3000/**", "https://final-step-fe-8-fs-2.vercel.app/**");
+//    registry.addMapping("/chat-ws").allowedMethods("GET", "OPTIONS")
+//      .allowedHeaders("*").allowedOrigins("http://localhost:3000", "https://final-step-fe-8-fs-2.vercel.app",
+//        "http://localhost:3000/**", "https://final-step-fe-8-fs-2.vercel.app/**");
+//
+//    registry.addMapping("/chat-ws/**").allowedMethods("GET", "OPTIONS")
+//      .allowedHeaders("*").allowedOrigins("http://localhost:3000", "https://final-step-fe-8-fs-2.vercel.app",
+//        "http://localhost:3000/**", "https://final-step-fe-8-fs-2.vercel.app/**");
+//
+//    registry.addMapping("/notifications-ws").allowedMethods("GET", "OPTIONS")
+//      .allowedHeaders("*").allowedOrigins("http://localhost:3000", "https://final-step-fe-8-fs-2.vercel.app",
+//        "http://localhost:3000/**", "https://final-step-fe-8-fs-2.vercel.app/**");
+//
+//    registry.addMapping("/notifications-ws/**").allowedMethods("GET", "OPTIONS")
+//      .allowedHeaders("*").allowedOrigins("http://localhost:3000", "https://final-step-fe-8-fs-2.vercel.app",
+//        "http://localhost:3000/**", "https://final-step-fe-8-fs-2.vercel.app/**");
 
-    registry.addMapping("/chat-ws/**").allowedMethods("GET", "OPTIONS")
-      .allowedHeaders("*").allowedOrigins("http://localhost:3000", "https://final-step-fe-8-fs-2.vercel.app",
-        "http://localhost:3000/**", "https://final-step-fe-8-fs-2.vercel.app/**");
-
-    registry.addMapping("/notifications-ws").allowedMethods("GET", "OPTIONS")
-      .allowedHeaders("*").allowedOrigins("http://localhost:3000", "https://final-step-fe-8-fs-2.vercel.app",
-        "http://localhost:3000/**", "https://final-step-fe-8-fs-2.vercel.app/**");
-
-    registry.addMapping("/notifications-ws/**").allowedMethods("GET", "OPTIONS")
-      .allowedHeaders("*").allowedOrigins("http://localhost:3000", "https://final-step-fe-8-fs-2.vercel.app",
-        "http://localhost:3000/**", "https://final-step-fe-8-fs-2.vercel.app/**");
-
-    registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+    registry.addMapping("/**")
+      .allowedOrigins("*")
+      .allowCredentials(false)
+      .maxAge(3600)
+      .allowedHeaders("Accept", "Content-Type", "Origin",
+        "Authorization", "X-Auth-Token")
+      .exposedHeaders("X-Auth-Token", "Authorization")
+      .allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS");
   }
 }
