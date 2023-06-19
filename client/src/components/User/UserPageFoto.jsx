7b9c95c6c9e6dbb31ec747e9ef085daca226ee9e
kostia89 <0665761301k@gmail.com>
2023-06-20 00:45:56 +0300

    fix foto

--- client/src/components/User/UserPageFoto.jsx
+++ client/src/components/User/UserPageFoto.jsx
@@ -1,14 +1,15 @@
 export function UserPageFoto({ hederImg }) {
   const defaultFoto =
-    "https://pibig.info/uploads/posts/2022-07/1657189991_21-pibig-info-p-temno-sinii-fon-odnotonnii-23.jpg";
+    'https://pibig.info/uploads/posts/2022-07/1657189991_21-pibig-info-p-temno-sinii-fon-odnotonnii-23.jpg';
   const fotoPage = hederImg || defaultFoto;
 
   return (
     <>
       <img
         style={{
-          width: "100%",
-          maxHeight: "200px",
+          width: '100%',
+          maxHeight: '200px',
+          objectFit: 'cover',
         }}
         src={fotoPage}
         alt="hederImg"
