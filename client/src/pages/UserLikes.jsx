4947f03121c72175b27150988b0b274dada0a4d3
kostia89 <0665761301k@gmail.com>
2023-06-20 00:14:08 +0300

    fix like

--- client/src/pages/UserLikes.jsx
+++ client/src/pages/UserLikes.jsx
@@ -13,6 +13,7 @@ import { getUserLikes } from 'src/redux/thunk/getUserLikes';
 import Post from 'src/components/Post/Post';
 import TweetPost from 'src/UI/tweet/TweetPost';
 import { useMode } from 'src/styles/_materialTheme';
+import TweetList from 'src/UI/TweetList';
 
 export const UserLikes = () => {
   const dispatch = useDispatch();
@@ -44,41 +45,42 @@ export const UserLikes = () => {
   // const userId = user.id;
 
   return (
-    us &&
-    us.map((likeTweet) => {
-      return (
-        <Box
-          key={likeTweet.tweet.id}
-          sx={{
-            borderBottom: `1px solid ${theme.palette.border.main}`,
-            transition: 'background-color 0.3s ease',
-            '&:hover': {
-              backgroundColor: `${theme.palette.background.hover}`,
-              cursor: 'pointer',
-            },
-          }}
-        >
-          <Link to={`/tweet/${likeTweet.tweet.id}`}>
-            <TweetPost
-              showIconList={true}
-              id={likeTweet.tweet.id}
-              displayName={likeTweet.tweet.user.fullName}
-              text={likeTweet.tweet.body}
-              username={likeTweet.tweet.user.userTag}
-              logoUrl={likeTweet.tweet.user.avatarImgUrl}
-              verified={likeTweet.tweet.user.isVerified}
-              images={likeTweet.tweet.attachmentImages}
-              likes={likeTweet.tweet.countLikes}
-              reply={likeTweet.tweet.countReplays}
-              retweet={likeTweet.tweet.countRetweets}
-              isLiked={likeTweet.tweet.currUserLiked}
-              isRetweet={likeTweet.tweet.countRetweets}
-              isComment={likeTweet.tweet.countReplays}
-            />
-          </Link>
-        </Box>
-      );
-    })
+    <TweetList tweets={us} />
+    // us &&
+    // us.map((likeTweet) => {
+    //   return (
+    //     <Box
+    //       key={likeTweet.tweet.id}
+    //       sx={{
+    //         borderBottom: `1px solid ${theme.palette.border.main}`,
+    //         transition: 'background-color 0.3s ease',
+    //         '&:hover': {
+    //           backgroundColor: `${theme.palette.background.hover}`,
+    //           cursor: 'pointer',
+    //         },
+    //       }}
+    //     >
+    //       <Link to={`/tweet/${likeTweet.tweet.id}`}>
+    //         <TweetPost
+    //           showIconList={true}
+    //           id={likeTweet.tweet.id}
+    //           displayName={likeTweet.tweet.user.fullName}
+    //           text={likeTweet.tweet.body}
+    //           username={likeTweet.tweet.user.userTag}
+    //           logoUrl={likeTweet.tweet.user.avatarImgUrl}
+    //           verified={likeTweet.tweet.user.isVerified}
+    //           images={likeTweet.tweet.attachmentImages}
+    //           likes={likeTweet.tweet.countLikes}
+    //           reply={likeTweet.tweet.countReplays}
+    //           retweet={likeTweet.tweet.countRetweets}
+    //           isLiked={likeTweet.tweet.currUserLiked}
+    //           isRetweet={likeTweet.tweet.countRetweets}
+    //           isComment={likeTweet.tweet.countReplays}
+    //         />
+    //       </Link>
+    //     </Box>
+    //   );
+    // })
   );
 };
 // <>
