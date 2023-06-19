3c11632e75ee77ec48210f45473a06c7153ba28b
kostia89 <0665761301k@gmail.com>
2023-06-20 00:39:51 +0300

    main

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -46,7 +46,6 @@ export const User = ({
         countUserFollowers={countUserFollowers}
       />
       <UserAction idUser={idUser} />
-      {/* <PostList /> */}
     </Box>
   );
 };
--- client/src/pages/UserLikes.jsx
+++ client/src/pages/UserLikes.jsx
@@ -1,17 +1,10 @@
 // UserLikes
 
-import { Box } from '@mui/material';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { ButtonSubscribe } from 'src/components/User/ButtonSubscribe';
-import { User } from 'src/components/User/User';
-import { getUserBiId } from 'src/redux/thunk/getUserBiId';
-import { Link, useParams } from 'react-router-dom';
-import PostList from 'src/components/Post/PostList';
-import { LinkToEditProfile } from 'src/components/User/LinkToEditProfile';
+import { useParams } from 'react-router-dom';
 import { getUserLikes } from 'src/redux/thunk/getUserLikes';
-import Post from 'src/components/Post/Post';
-import TweetPost from 'src/UI/tweet/TweetPost';
+
 import { useMode } from 'src/styles/_materialTheme';
 import TweetList from 'src/UI/TweetList';
 
@@ -67,53 +60,3 @@ export const UserLikes = () => {
     // })
   );
 };
-// <>
-// asd
-{
-  /* <Box
-        sx={{
-          display: 'flex',
-
-          direction: 'column',
-        }}
-      >
-        <User
-          lincToFollowers={lincToFollowers}
-          lincToFollowings={lincToFollowings}
-          userButton={subscribe}
-          fullName={user.fullName}
-          tweetsCounter={user.countUserTweets}
-          hederImg={user.headerImgUrl}
-          userAvatar={user.avatarImgUrl}
-          userTag={user.userTag}
-          userBio={user.bio}
-          userLocation={user.location}
-          createdAt={user.createdAt}
-          countUserFollowings={user.countUserFollowings}
-          countUserFollowers={user.countUserFollowers}
-        />
-      </Box> */
-}
-
-{
-  /* <Post
-        showIconList={false}
-        id={userLikes.id}
-        displayName={userLikes.user.fullName}
-        text={userLikes.body}
-        username={userLikes.user.userTag}
-        logoUrl={userLikes.user.avatarImgUrl}
-        verified={userLikes.user.isVerified}
-        images={userLikes.attachmentImages}
-        likes={userLikes.countLikes}
-        reply={userLikes.countReplays}
-        retweet={userLikes.countRetweets}
-        isLiked={userLikes.currUserLiked}
-        isRetweet={userLikes.countRetweets}
-        isComment={userLikes.countReplays}
-      /> */
-}
-{
-  /* </> */
-}
-// );
