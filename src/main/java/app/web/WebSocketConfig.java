bb5deac0d71575c238eb1be168229214981951ad
Pavel Bortnovskyi <bortnovskyi.pavlo@gmail.com>
2023-06-23 21:47:14 +0300

    Subscription check removed

--- src/main/java/app/web/WebSocketConfig.java
+++ src/main/java/app/web/WebSocketConfig.java
@@ -103,14 +103,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         String token = jwtTokenService.extractTokenFromHeader(Objects.requireNonNull(accessor.getFirstNativeHeader("Authorization")))
           .orElseThrow(() -> new JwtAuthenticationException("Token not found!"));
         if (jwtTokenService.validateToken(token, TokenType.ACCESS)) {
-          if (accessor.getCommand().equals(StompCommand.SUBSCRIBE)) {
-            String userName = jwtTokenService.extractUserNameFromClaims(jwtTokenService.extractClaimsFromToken(token, TokenType.ACCESS).get()).get();
-
-            if (!(destination.equals("/topic/сhats/" + userName)) || destination.equals("/topic/notifications/" + userName)) {
-              log.info("Attempt to subscribe to other user channel: " + destination);
-              throw new JwtAuthenticationException("Attempt to subscribe to other user channel: " + destination);
-            }
-          }
+//          if (accessor.getCommand().equals(StompCommand.SUBSCRIBE)) {
+//            String userName = jwtTokenService.extractUserNameFromClaims(jwtTokenService.extractClaimsFromToken(token, TokenType.ACCESS).get()).get();
+//
+//            if (!(destination.equals("/topic/сhats/" + userName)) || destination.equals("/topic/notifications/" + userName)) {
+//              log.info("Attempt to subscribe to other user channel: " + destination);
+//              throw new JwtAuthenticationException("Attempt to subscribe to other user channel: " + destination);
+//            }
+//          }
           processWebSocketRequestWithToken(token, accessor);
           log.info("Token:" + token);
           log.info("UserId: " + jwtTokenService.extractIdFromClaims(jwtTokenService.extractClaimsFromToken(token, TokenType.ACCESS).get()).get().toString());
