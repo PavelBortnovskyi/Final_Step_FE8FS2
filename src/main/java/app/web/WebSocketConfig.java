59cb2802cb141efde901accd6792c06cff398fc2
Pavel Bortnovskyi <bortnovskyi.pavlo@gmail.com>
2023-06-26 10:52:10 +0300

    user subscribe attempt

--- src/main/java/app/web/WebSocketConfig.java
+++ src/main/java/app/web/WebSocketConfig.java
@@ -69,7 +69,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableSimpleBroker("/user")
+    registry.enableSimpleBroker("/user/")
       .setHeartbeatValue(new long[] {10000, 10000})
       .setTaskScheduler(messageBrokerTaskScheduler);
     registry.setApplicationDestinationPrefixes("/api");
