064c3f300ee34657b3614a6163779a823897cad9
Pavel Bortnovskyi <bortnovskyi.pavlo@gmail.com>
2023-06-30 15:42:16 +0300

    OAuth login page removed

--- src/main/java/app/controller/OAuth2TestController.java
+++ src/main/java/app/controller/OAuth2TestController.java
@@ -1,17 +1,17 @@
-package app.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-@RequestMapping(value = "/")
-public class OAuth2TestController {
-
-  //http://localhost:8080/login
-  @GetMapping("/login")
-  private ModelAndView handleGetOAuth2Login() {
-    return new ModelAndView("login");
-  }
-}
+//package app.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//@RequestMapping(value = "/")
+//public class OAuth2TestController {
+//
+//  //http://localhost:8080/login
+//  @GetMapping("/login")
+//  private ModelAndView handleGetOAuth2Login() {
+//    return new ModelAndView("login");
+//  }
+//}
